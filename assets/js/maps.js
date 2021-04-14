c6c55fb16945fd986c3370f2dfbd3f4252758f44
GregKaighin <gregkaighin@gmail.com>
2021-04-14 21:50:14 +0000

    Add viewport

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -4,7 +4,7 @@ const bedfordshire = {
     lat: 52.02973,
     lng: -0.45303
 };
-var mapOtions = {
+const mapOtions = {
     center: bedfordshire,
     zoom: 10,
     mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -13,16 +13,15 @@ var mapOtions = {
 //Create the map with options set
 const map = new google.maps.Map(document.getElementById("googleMap"), mapOtions);
 
+//Create the bicycling layer
 const bikeLayer = new google.maps.BicyclingLayer();
 bikeLayer.setMap(map);
-
 
 //Create a DirectionsService object to use the route method and get a result for our request
 var directionsService = new google.maps.DirectionsService();
 
 //Create a DirectionsRenderer object which we will use to display the route
 var directionsDisplay = new google.maps.DirectionsRenderer();
-
 
 //Bind the DirectionsRenderer to the map
 directionsDisplay.setMap(map);
@@ -36,12 +35,11 @@ function calcRoute() {
         travelMode: google.maps.TravelMode.BICYCLING, //WALKING, BICYCLING, TRANSIT
         unitSystem: google.maps.UnitSystem.IMPERIAL
     }
-
     // Pass the request to the route method
     directionsService.route(request, function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
 
-            // Get distance and time
+            // Get the route distance and time
             const output = document.querySelector('#output');
             output.innerHTML = "<div class='alert-info'>From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ".<br /> Cycling distance <i class='fas fa-biking'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-stopwatch'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
 
@@ -52,26 +50,30 @@ function calcRoute() {
             directionsDisplay.setDirections({
                 routes: []
             });
-            // Center map on Bedfordshire
+            // Recenter map on Bedfordshire
             map.setCenter(bedfordshire);
 
             // Show error message if the route is not possible
-            output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
+            output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> This route is not possible on a bicycle!</div>";
         }
     });
 
 }
 
-// Create searchBox objects for all inputs
+// Create searchBox1 object for starting place
 var input1 = document.getElementById("from");
 var searchBox1 = new google.maps.places.SearchBox(input1);
-// Bias the SearchBox results towards current map's viewport.
+
+// Bias the SearchBox1 results towards current map's viewport.
 map.addListener("bounds_changed", () => {
     searchBox1.setBounds(map.getBounds());
 });
+
+// Create searchBox2 object for destination
 var input2 = document.getElementById("to");
 var searchBox2 = new google.maps.places.SearchBox(input2);
-// Bias the SearchBox results towards current map's viewport.
+
+// Bias the SearchBox2 results towards current map's viewport.
 map.addListener("bounds_changed", () => {
     searchBox2.setBounds(map.getBounds());
 });
