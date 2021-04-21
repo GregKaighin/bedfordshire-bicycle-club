8f7892c924b0428363bcfbc384d4e825df2dc83b
GregKaighin <gregkaighin@gmail.com>
2021-04-21 00:08:57 +0000

    Style the navbar colours and terms text on the map

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -4,11 +4,90 @@ const bedfordshire = {
     lng: -0.45303
 };
 
+var stylesArray = [{
+        "featureType": "water",
+        "stylers": [{
+                "visibility": "on"
+            },
+            {
+                "color": "#b5cbe4"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "stylers": [{
+            "color": "#efefef"
+        }]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#83a5b0"
+        }]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#bdcdd3"
+        }]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "lightgreen"
+        }]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#e3eed3"
+        }]
+    },
+    {
+        "featureType": "administrative",
+        "stylers": [{
+                "visibility": "on"
+            },
+            {
+                "lightness": 33
+            }
+        ]
+    },
+    {
+        "featureType": "road"
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels",
+        "stylers": [{
+                "visibility": "on"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {},
+    {
+        "featureType": "road",
+        "stylers": [{
+            "lightness": 20
+        }]
+    }
+]
+
 // Set the map options
 const mapOtions = {
     center: bedfordshire,
     zoom: 10,
+    styles: stylesArray,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
+    // Disable the default map UI, enable the zoom control
     disableDefaultUI: true,
     zoomControl: true
 };
--- assets/js/stores.js
+++ assets/js/stores.js
@@ -5,6 +5,83 @@ let infoWindow;
 let currentInfoWindow;
 let service;
 
+var stylesArray = [{
+        "featureType": "water",
+        "stylers": [{
+                "visibility": "on"
+            },
+            {
+                "color": "#b5cbe4"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "stylers": [{
+            "color": "#efefef"
+        }]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#83a5b0"
+        }]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#bdcdd3"
+        }]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "lightgreen"
+        }]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#e3eed3"
+        }]
+    },
+    {
+        "featureType": "administrative",
+        "stylers": [{
+                "visibility": "on"
+            },
+            {
+                "lightness": 33
+            }
+        ]
+    },
+    {
+        "featureType": "road"
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels",
+        "stylers": [{
+                "visibility": "on"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {},
+    {
+        "featureType": "road",
+        "stylers": [{
+            "lightness": 20
+        }]
+    }
+]
+
 function initMap() {
     // Initialize variables
     bounds = new google.maps.LatLngBounds();
@@ -18,7 +95,8 @@ function initMap() {
     map = new google.maps.Map(document.getElementById('googleMap'), {
         center: bedfordshire,
         zoom: 10,
-        // Disable default UI, add the zoom control
+        styles: stylesArray,
+        // Disable the default map UI, enable the zoom control
         disableDefaultUI: true,
         zoomControl: true
     });
