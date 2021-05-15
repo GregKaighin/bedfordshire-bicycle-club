2f802364b389d1ee673d4e1ef5ca606de0adaada
GregKaighin <gregkaighin@gmail.com>
2021-05-15 17:18:49 +0000

    Resize jpeg images

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -302,74 +302,23 @@ $(document).ready(function () {
     }
 });
 
-function presetRoutes() {
-    // Create waypoints variable array and loop and push to route request
-    var waypts = [];
-    var waypointElmts = document.getElementsByName('waypoints[]');
-    for (var i = 0; i < waypointElmts.length; i++) {
-        if (waypointElmts[i].value.length > 0) {
-            waypts.push({
-                location: waypointElmts[i].value,
-                stopover: true
-            });
-        }
-    }
-    var prioryMarinaSandy = {
-        origin: {
-            lat: 52.1472,
-            lng: -0.4649
-        },
-        destination: {
-            lat: 52.1588,
-            lng: -0.3914
-        },
-        travelMode: google.maps.TravelMode.BICYCLING,
-        unitSystem: google.maps.UnitSystem.IMPERIAL
-    };
-    var prioryMarinaSandy = {
-        origin: {
-            lat: 52.1472,
-            lng: -0.4649
-        },
-        destination: {
-            lat: 52.1588,
-            lng: -0.3914
-        },
-        travelMode: google.maps.TravelMode.BICYCLING,
-        unitSystem: google.maps.UnitSystem.IMPERIAL
-    }; var prioryMarinaSandy = {
-        origin: {
-            lat: 52.1472,
-            lng: -0.4649
-        },
-        destination: {
-            lat: 52.1588,
-            lng: -0.3914
-        },
-        travelMode: google.maps.TravelMode.BICYCLING,
-        unitSystem: google.maps.UnitSystem.IMPERIAL
-    }
-    // Create route request
+// Recommended routes
+//Functions for recommended routes
+function prioryMarinaSandy() {
+    // Create a route request
     var request = {
-        origin: document.getElementById('start').value,
-        destination: document.getElementById('end').value,
+        origin: {
+            lat: 52.131972,
+            lng: -0.434981
+        },
+        destination: {
+            lat: 52.1281,
+            lng: -0.2868
+        },
         travelMode: google.maps.TravelMode.BICYCLING,
-        unitSystem: google.maps.UnitSystem.IMPERIAL,
-        waypoints: waypts,
-        // Calculate route in specified order
-        optimizeWaypoints: false,
-    };
-    document.getElementByClassName('preset-route-buttons').addEventListener('click', function (e) {
-        var target = e.target;
-        if (target.id === 'prioryMarinaSandy') {
-            request = prioryMarinaSandy;
-        } else if (target.id === 'blueLagoonFlitwick') {
-            request = blueLagoonFlitwick;
-        } else {
-            request = bedfordParkRenhold;
-        }
-    }, false);
-    // Pass request to route method
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+    }
+    // Pass the request to the .route method
     directionsService.route(request, function (response, status) {
         if (status == google.maps.DirectionsStatus.OK) {
             directionsDisplay.setDirections(response);
@@ -381,10 +330,92 @@ function presetRoutes() {
             // Updates route summary panel when directions change
             directionsDisplay.addListener("directions_changed", () => {
                 computeTotalDistAndTime(directionsDisplay.getDirections());
+
+
             });
         } else {
             var routeSummary = document.querySelector('#route-summary');
-            routeSummary.innerHTML = '<div class="alert-danger"><i class="fas fa-exclamation-triangle"></i> Please enter a valid route!</div>';
+            routeSummary.innerHTML = '<div class="alert-danger"><i class="fas fa-exclamation-triangle"></i> Route unavailable!</div>';
+            // Clear map
+            directionsDisplay.setMap();
+            // Clear directions panel 
+            directionsDisplay.setPanel();
+        }
+    });
+}
+
+function blueLagoonFlitwick() {
+    // Create a route request
+    var request = {
+        origin: {
+            lat: 51.9941,
+            lng: -0.2580
+        },
+        destination: {
+            lat: 52.0046,
+            lng: -0.4979
+        },
+        travelMode: google.maps.TravelMode.BICYCLING,
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+    }
+    // Pass the request to the .route method
+    directionsService.route(request, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+            computeTotalDistAndTime(response);
+            // Display directions on map
+            directionsDisplay.setMap(map);
+            //Display directions panel
+            directionsDisplay.setPanel(document.getElementById("directions-panel"));
+            // Updates route summary panel when directions change
+            directionsDisplay.addListener("directions_changed", () => {
+                computeTotalDistAndTime(directionsDisplay.getDirections());
+
+
+            });
+        } else {
+            var routeSummary = document.querySelector('#route-summary');
+            routeSummary.innerHTML = '<div class="alert-danger"><i class="fas fa-exclamation-triangle"></i> Route unavailable!</div>';
+            // Clear map
+            directionsDisplay.setMap();
+            // Clear directions panel 
+            directionsDisplay.setPanel();
+        }
+    });
+}
+
+function bedfordParkRenhold() {
+    // Create a route request
+    var request = {
+        origin: {
+            lat: 52.1472,
+            lng: -0.4649
+        },
+        destination: {
+            lat: 52.1588,
+            lng: -0.3914
+        },
+        travelMode: google.maps.TravelMode.BICYCLING,
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+    }
+    // Pass the request to the .route method
+    directionsService.route(request, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+            computeTotalDistAndTime(response);
+            // Display directions on map
+            directionsDisplay.setMap(map);
+            //Display directions panel
+            directionsDisplay.setPanel(document.getElementById("directions-panel"));
+            // Updates route summary panel when directions change
+            directionsDisplay.addListener("directions_changed", () => {
+                computeTotalDistAndTime(directionsDisplay.getDirections());
+
+
+            });
+        } else {
+            var routeSummary = document.querySelector('#route-summary');
+            routeSummary.innerHTML = '<div class="alert-danger"><i class="fas fa-exclamation-triangle"></i> Route unavailable!</div>';
             // Clear map
             directionsDisplay.setMap();
             // Clear directions panel 
