89af2fd13c90e0e1a6ea20f46e7def50bf3c6241
GregKaighin <gregkaighin@gmail.com>
2021-05-16 23:15:23 +0000

    Fix contact form

---
+++ assets/js/emailjs.js
@@ -0,0 +1,19 @@
+function sendMail(contactForm) {
+    emailjs.send("default_service", "gkpianolessons", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "phone": contactForm.phone.value,
+        "message": contactForm.message.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                $('#thank-you-modal').modal('show');  // Opens the modal on submission of all completed form fields
+                $('#form-reset')[0].reset(); // Resets form fields on submission
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // Stops the page from refreshing
+}
